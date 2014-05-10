83e46ee6483ce52a0f8c7510fd2dda106dab44d5
Vitaly Babiy <vbabiy86@gmail.com>
2014-05-10 10:59:38 -0400

    Use pg_class to make count for page count faster.

--- botbot/apps/logs/admin.py
+++ botbot/apps/logs/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
 from . import models
+from botbot.core.paginator import PostgresLargeTablePaginator
 
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'command', 'bot', 'timestamp']
     list_filter = ['bot', 'command']
+    paginator = PostgresLargeTablePaginator
 
 
 admin.site.register(models.Log, LogAdmin)
