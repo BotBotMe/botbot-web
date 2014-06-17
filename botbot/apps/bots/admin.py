9be43af8a5754af96feb02e972f0e56945374651
Yann Malet <yann.malet@gmail.com>
2014-06-17 21:45:18 +0200

    Reload botbot-configuration is now an admin action

--- botbot/apps/bots/admin.py
+++ botbot/apps/bots/admin.py
@@ -1,12 +1,12 @@
 """Django admin configuration for the bot objects.
 """
 from django.contrib import admin
-from django.db import transaction
 from django.forms.models import BaseInlineFormSet
 import redis
 
 from . import models
 from botbot.apps.plugins.models import Plugin
+
 
 class PluginFormset(BaseInlineFormSet):
     def __init__(self, *args, **kwargs):
@@ -42,53 +42,26 @@ class ChatBotAdmin(admin.ModelAdmin):
     # Disable bulk delete, because it doesn't call delete, so skips REFRESH
     actions = None
 
-    def save_model(self, request, obj, form, change):
-        super(ChatBotAdmin, self).save_model(request, form, form, change)
-        transaction.commit()
-        daemon_refresh()
 
-    def delete_model(self, request, obj):
-        super(ChatBotAdmin, self).delete_model(request, obj)
-        transaction.commit()
-        daemon_refresh()
-
-
-class ChannelAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'name', 'chatbot', 'is_active',
-                    'is_public', 'is_featured', 'is_pending', 'updated')
-    list_filter = ('chatbot', 'is_active', 'is_public', 'is_featured', 'is_pending')
-    list_editable = ('is_active',)
-    readonly_fields = ('fingerprint', 'created', 'updated')
-    search_fields = ('name', 'chatbot__server')
-    inlines = [
-            ActivePluginInline,
-            MembershipInline
-        ]
-
-    def save_model(self, request, obj, form, change):
-        """
-        Given a model instance save it to the database.
-        """
-        super(ChannelAdmin, self).save_model(request, obj, form, change)
-        transaction.commit()
-        daemon_refresh()
-
-    def delete_model(self, request, obj):
-        """
-        Given a model instance delete it from the database.
-        """
-        super(ChannelAdmin, self).delete_model(request, obj)
-        transaction.commit()
-        daemon_refresh()
-        
-
-def daemon_refresh():
+def botbot_refresh(modeladmin, request, queryset):
     """
     Ask daemon to reload configuration
     """
     queue = redis.Redis(db=0)
     queue.lpush('bot', 'REFRESH')
+botbot_refresh.short_description = "Reload botbot-bot configuration"
 
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'name', 'chatbot', 'is_active',
+                    'is_public', 'is_featured', 'is_pending', 'updated')
+    list_filter = ('chatbot', 'is_active', 'is_public',
+                   'is_featured', 'is_pending')
+    list_editable = ('is_active',)
+    readonly_fields = ('fingerprint', 'created', 'updated')
+    search_fields = ('name', 'chatbot__server')
+    inlines = [ActivePluginInline, MembershipInline]
+    actions = [botbot_refresh]
 
 admin.site.register(models.ChatBot, ChatBotAdmin)
 admin.site.register(models.Channel, ChannelAdmin)
