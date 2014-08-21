ee0ca11cac0d99c13b51ee7dfc241827262cc7fe
Vitaly Babiy <vbabiy86@gmail.com>
2014-08-21 15:08:14 -0400

    Added static command list.

--- botbot/apps/logs/admin.py
+++ botbot/apps/logs/admin.py
@@ -4,9 +4,35 @@ from . import models
 from botbot.core.paginator import PostgresLargeTablePaginator
 
 
+class CommandListFilter(admin.SimpleListFilter):
+    title = "Command"
+    parameter_name = "command"
+
+    def lookups(self, request, model_admin):
+        return (
+            ('ACTION', 'ACTION'),
+            ('ERROR', 'ERROR'),
+            ('JOIN', 'JOIN'),
+            ('KICK', 'KICK'),
+            ('MODE', 'MODE'),
+            ('NICK', 'NICK'),
+            ('NOTICE', 'NOTICE'),
+            ('PART', 'PART'),
+            ('PING', 'PING'),
+            ('PRIVMSG', 'PRIVMSG'),
+            ('QUIT', 'QUIT'),
+            ('SHUTDOWN', 'SHUTDOWN'),
+            ('TOPIC', 'TOPIC'),
+            ('VERSION', 'VERSION'),
+        )
+
+    def queryset(self, request, queryset):
+        return queryset.filter(command=self.value())
+
+
 class LogAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'command', 'bot', 'timestamp']
-    list_filter = ['bot', 'command']
+    list_filter = ['bot', CommandListFilter]
     paginator = PostgresLargeTablePaginator
 
 
