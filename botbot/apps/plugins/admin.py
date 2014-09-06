86c9126533886141a70422fd197e28573db1acc8
Yann Malet <yann.malet@gmail.com>
2014-09-06 11:48:27 +0200

    Adding an admin reflection of the ActivePlugin for mass edition

--- botbot/apps/plugins/admin.py
+++ botbot/apps/plugins/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from . import models
 
+class ActivePluginAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'configuration')
+    list_filter = ('channel', 'plugin')
+    list_editable = ('configuration',)
+
 admin.site.register(models.Plugin)
+admin.site.register(models.ActivePlugin, ActivePluginAdmin)
