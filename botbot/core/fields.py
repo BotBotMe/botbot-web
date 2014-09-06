71ae2f1ba8ff0a590e60eae148762d623d5b5563
Yann Malet <yann.malet@gmail.com>
2014-09-06 13:41:53 +0200

    Adding value_from_object to the JsonField

--- botbot/core/fields.py
+++ botbot/core/fields.py
@@ -3,6 +3,11 @@ import json
 from django.db import models
 from django.core.serializers.json import DjangoJSONEncoder
 
+from django.forms import fields
+from django.forms.util import ValidationError
+
+
+
 class JSONField(models.TextField):
     """JSONField is a generic textfield that neatly serializes/unserializes
     JSON objects seamlessly"""
@@ -12,29 +17,27 @@ class JSONField(models.TextField):
 
     def to_python(self, value):
         """Convert our string value to JSON after we load it from the DB"""
-
         if value == "":
             return None
-
         try:
             if isinstance(value, basestring):
                 return json.loads(value)
         except ValueError:
             pass
-
         return value
 
-    def get_db_prep_save(self, value, connection):
-        """Convert our JSON object to a string before we save"""
-
+    def get_db_prep_save(self, value, *args, **kwargs):
         if value == "":
             return None
-
-        if isinstance(value, dict):
+        if isinstance(value, dict) or isinstance(value, list):
             value = json.dumps(value, cls=DjangoJSONEncoder)
+            return super(JSONField, self).get_db_prep_save(value, *args, **kwargs)
 
-        return super(JSONField, self).get_db_prep_save(value, connection)
-
+    def value_from_object(self, obj):
+        value = super(JSONField, self).value_from_object(obj)
+        if self.null and value is None:
+            return None
+        return json.dumps(value)
 
 try:
     from south.modelsinspector import add_introspection_rules
