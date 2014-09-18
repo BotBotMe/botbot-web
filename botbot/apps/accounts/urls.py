cdeb186c9250b7da2765366d1178a496cece0b2e
Vitaly Babiy <vbabiy86@gmail.com>
2014-09-18 15:35:30 -0400

    Switch from python-social-auth to django-allauth. django-allauth better support the workflow we want.

--- botbot/apps/accounts/urls.py
+++ botbot/apps/accounts/urls.py
@@ -2,17 +2,8 @@ from django.conf.urls import patterns, url, include
 
 from . import views
 
-urlpatterns = patterns('django.contrib.auth.views',
-    url('login/$', 'login', {'template_name': 'accounts/login.html'}, 'login'),
-    url('logout/$', 'logout', {'next_page': '/'},
-        'logout'),
-    url('password/reset/$', 'password_reset',
-        name='password_reset'),
-    url('password/sent/$', 'password_reset_done',
-        name='password_reset_done'),
-    (r'', include('django.contrib.auth.urls')),
-    url('', include('social.apps.django_app.urls', namespace='social')),
-
-    url('manage/$', views.ManageAccount.as_view(), name='account_manage'),
-    url('_timezone/$', views.SetTimezone.as_view(), name="_set_timezone"),
+urlpatterns = patterns('',
+    url(r'manage/$', views.ManageAccount.as_view(), name='account_manage'),
+    url(r'_timezone/$', views.SetTimezone.as_view(), name="_set_timezone"),
+    url(r'', include('allauth.urls')),
 )
