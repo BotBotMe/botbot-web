ddcdb701535cb24386eb215a89a30da9d021115c
Yann Malet <yann.malet@gmail.com>
2014-09-18 10:20:44 +0200

    We have some catch all url in the urls/base.py that really need to be at the end else they interfere with other urls

--- botbot/urls/base.py
+++ botbot/urls/base.py
@@ -16,24 +16,6 @@ urlpatterns = patterns('django.shortcuts',
     url(r'^privacy/$', 'render', {'template_name': 'privacy.html'}),
 )
 
-urlpatterns += patterns('',
-    (r'^$', LandingPage.as_view()),
-    (r'', include('launchpad.urls')),
-    url(r'^dashboard/$', Dashboard.as_view(), name='dashboard'),
-    url(r'^add/$', AddChannel.as_view(), name="add_channel"),
-    url(r'^request/$', RequestChannel.as_view(), name='request_channel'),
-    url(r'^request/success/$', 'django.shortcuts.render',
-        {'template_name': 'bots/request_success.html'}, name='request_channel_success'),
-
-    (r'^accounts/', include('botbot.apps.accounts.urls')),
-    url(r'^_suggest_users/$', SuggestUsers.as_view(), name='suggest_users'),
-
-    url(r'^(?P<bot_slug>[\-\w\:\.]+(\@[\w]+)?)/(?P<channel_slug>[\-\w\.]+)/',
-        include(channel_patterns)),
-    url(r'^(?P<network_slug>[\-\w\.]+)/$', ChannelList.as_view())
-)
-
-
 if settings.INCLUDE_DJANGO_ADMIN:
     from .admin import urlpatterns as admin_urlpatterns
     # Prepend the admin urls.
@@ -51,3 +33,21 @@ if settings.DEBUG:
         url(r'^404/$', 'render', {'template_name': '404.html'}),
         url(r'^500/$', 'render', {'template_name': '500.html'}),
     )
+
+urlpatterns += patterns('',
+    (r'^$', LandingPage.as_view()),
+    (r'', include('launchpad.urls')),
+    url(r'^dashboard/$', Dashboard.as_view(), name='dashboard'),
+    url(r'^add/$', AddChannel.as_view(), name="add_channel"),
+    url(r'^request/$', RequestChannel.as_view(), name='request_channel'),
+    url(r'^request/success/$', 'django.shortcuts.render',
+        {'template_name': 'bots/request_success.html'}, name='request_channel_success'),
+
+    (r'^accounts/', include('botbot.apps.accounts.urls')),
+    url(r'^_suggest_users/$', SuggestUsers.as_view(), name='suggest_users'),
+
+    url(r'^(?P<bot_slug>[\-\w\:\.]+(\@[\w]+)?)/(?P<channel_slug>[\-\w\.]+)/',
+        include(channel_patterns)),
+    url(r'^(?P<network_slug>[\-\w\.]+)/$', ChannelList.as_view())
+)
+
