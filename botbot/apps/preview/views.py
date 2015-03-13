eabcccd62359917e00d3b6773001d0546e87e47b
Vitaly Babiy <vbabiy86@gmail.com>
2015-03-13 15:17:00 -0400

    Don't show non active rooms.

--- botbot/apps/preview/views.py
+++ botbot/apps/preview/views.py
@@ -4,14 +4,13 @@ from botbot.apps.bots import models as bots_models
 
 
 class LandingPage(Signup):
-
     def get_context_data(self, **kwargs):
         kwargs.update({
-            'featured_channels': bots_models.Channel.objects\
-                .filter(is_public=True, is_featured=True)\
+            'featured_channels': bots_models.Channel.objects \
+                .filter(is_public=True, is_featured=True, is_active=True) \
                 .select_related('chatbot'),
-            'public_not_featured_channels': bots_models.Channel.objects\
-                .filter(is_public=True, is_featured=False)\
+            'public_not_featured_channels': bots_models.Channel.objects \
+                .filter(is_public=True, is_featured=False, is_active=True) \
                 .select_related('chatbot')
         })
         return kwargs
