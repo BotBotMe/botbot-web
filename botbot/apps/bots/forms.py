3ff388e9c4b1f80f6837faf067428a2970baa030
Michael Trythall <m@mtrythall.com>
2015-08-20 14:56:54 -0400

    Updates request form with less fields and some explanatory copy.

--- botbot/apps/bots/forms.py
+++ botbot/apps/bots/forms.py
@@ -29,8 +29,6 @@ class ChannelRequestForm(forms.Form):
     channel_name = forms.CharField()
     server = forms.ChoiceField(choices=[],
                                label="IRC Server")
-    connection = forms.CharField(required=False, label="New IRC Server",
-                                 help_text="IRC Server should be specified as <url>:<port>.")
     github = forms.URLField(label="GitHub Repo URL",
                             help_text="If the channel supports a github repo, the url to the repo.",
                             required=False)
@@ -57,66 +55,7 @@ class ChannelRequestForm(forms.Form):
     def _set_server_choices(self):
         choices = [(c.pk, c.server) for c in ChatBot.objects.filter(is_active=True)]
         choices.insert(0, ('', '---------'))
-        choices.append(("new", "Don't see mine, let me add it."))
         self.fields['server'].choices = choices
-
-    def clean_channel_name(self):
-        channel_name = self.cleaned_data['channel_name']
-        if not channel_name.startswith("#"):
-            channel_name = "#" + channel_name
-
-        try:
-            channel = models.Channel.objects.filter(name=channel_name)[0]
-            if channel.is_active:
-                msg = mark_safe('<a href="{}">{}</a> is already being '
-                                'monitored.'.format(channel.get_absolute_url(),
-                                                    channel_name))
-            else:
-                msg = ('This channel is already in the request queue. Please '
-                       'be patient while we process the request.')
-            raise forms.ValidationError(msg)
-        except IndexError:
-            pass
-
-
-        return channel_name
-
-    def clean_connection(self):
-        connection = self.cleaned_data['connection']
-        if connection and not connection_regex.match(connection):
-            raise forms.ValidationError(
-                "Incorrect format, should be <url>:<port>")
-
-        return connection
-
-    def clean_server(self):
-        """
-        Make sure server data is clean.
-
-        :return: ChatBot if it was selected, None if the user should have gave
-        us input to create a new one. Validation error if ChatBot was not found.
-        """
-        pk = self.cleaned_data['server']
-        if pk == "new":
-            return None
-
-        try:
-            return ChatBot.objects.get(pk=pk)
-        except ChatBot.DoesNotExist:
-            raise forms.ValidationError("Server doesn't exist.")
-
-    def clean(self):
-        cleaned_data = super(ChannelRequestForm, self).clean()
-        server = cleaned_data.get('server')
-        connection = cleaned_data.get('connection')
-        if not self._errors.get('connection'):
-            if server is None and not connection:
-                self._errors["connection"] = self.error_class([
-                    "This field is required."
-                ])
-
-        return cleaned_data
-
 
 class UsersForm(forms.Form):
     users = forms.ModelMultipleChoiceField(required=False,
