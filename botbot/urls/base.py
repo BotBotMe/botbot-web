3789873571af6f586ec2f2bb3f21cc7f273d6c34
Michael Trythall <m@mtrythall.com>
2015-10-27 16:38:10 -0400

    Shuffles URLs

--- botbot/urls/base.py
+++ botbot/urls/base.py
@@ -8,18 +8,16 @@ channel_patterns = patterns('',
     url(r'', include('botbot.apps.logs.urls')),
 )
 
-urlpatterns = patterns('django.shortcuts',
-    url(r'^terms/$', 'render', {'template_name': 'terms.html'},
-        name='terms'),
-    url(r'^privacy/$', 'render', {'template_name': 'privacy.html'},
-        name='privacy'),
-    url(r'^how-to-setup-irc-channel/$', 'render', {
-        'template_name': 'howto.html'},
-        name='how-to'),
-    url(r'^irc-guide/$', 'render', {
-        'template_name': 'irc-guide.html'},
-        name='irc-guide'),
-)
+urlpatterns = patterns('',
+                        (r'^$', LandingPage.as_view()),
+                        url(r'^sitemap\.xml$', include('botbot.apps.sitemap.urls')),
+
+                        url(r'^_suggest_users/$', SuggestUsers.as_view(), name='suggest_users'),
+
+                        url(r'^(?P<bot_slug>[\-\w\:\.]+(\@[\w]+)?)/(?P<channel_slug>[\-\w\.]+)/',
+                            include(channel_patterns)),
+                        url(r'^(?P<network_slug>[\-\w\.]+)/$', ChannelList.as_view())
+                        )
 
 if settings.INCLUDE_DJANGO_ADMIN:
     from .admin import urlpatterns as admin_urlpatterns
@@ -31,15 +29,3 @@ if settings.DEBUG:
         url(r'^404/$', 'render', {'template_name': '404.html'}),
         url(r'^500/$', 'render', {'template_name': '500.html'}),
     )
-
-urlpatterns += patterns('',
-                        (r'^$', LandingPage.as_view()),
-                        (r'', include('launchpad.urls')),
-                        url(r'^sitemap\.xml$', include('botbot.apps.sitemap.urls')),
-
-                        url(r'^_suggest_users/$', SuggestUsers.as_view(), name='suggest_users'),
-
-                        url(r'^(?P<bot_slug>[\-\w\:\.]+(\@[\w]+)?)/(?P<channel_slug>[\-\w\.]+)/',
-                            include(channel_patterns)),
-                        url(r'^(?P<network_slug>[\-\w\.]+)/$', ChannelList.as_view())
-                        )
